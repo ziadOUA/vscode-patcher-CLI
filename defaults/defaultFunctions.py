a58d399fde0e56c14428c6cbe1cd743dddba8067
ziadOUA <ziadouapro@outlook.com>
2023-02-14 21:40:33 +0100

    Moved the 3 functions regarding the status of the actions in `defaultFunctions.py`

--- defaults/defaultFunctions.py
+++ defaults/defaultFunctions.py
@@ -1,2 +1,17 @@
+from colorama import Fore
+
+
 def new_line():
     print('')
+
+
+def success_msg():
+    print(f'{Fore.GREEN} Success{Fore.RESET}')
+
+
+def failure_msg():
+    print(f'{Fore.RED} Failed{Fore.RESET}')
+
+
+def file_status_msg(action, file_name):
+    print(f'{action} {file_name}...', end='')
